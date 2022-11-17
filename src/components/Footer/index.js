87eb020728f6978aa6bb70c881be4353fc7b18e2
mienblack <damienfelipe2015@gmail.com>
2022-11-17 01:55:20 -0300

    linkedin link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ export default function Footer() {
                 <div className="div-footer">
                     <p>Made with 💟 by Damien Costa ✌🏿</p>
                     <a href="https://github.com/mienblack" target="_blank"><img src="/assets/images/github-icon.png"  alt="github" /></a>
-                    <a href="https://www.linkedin.com/in/damien-costa-969953164/" target="_blank"><img src="/assets/images/linkedin-icon.png" alt="linkedin" /></a>
+                    <a href="https://www.linkedin.com/in/damien-costa/" target="_blank"><img src="/assets/images/linkedin-icon.png" alt="linkedin" /></a>
                 </div>
             </section>
         </StyledFooter>
